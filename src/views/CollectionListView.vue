e78438468772fea6be02e6f52ddbf26188d1b3fc
sploit <acidsploit@users.noreply.github.com>
2023-10-21 17:11:32 +0200

    add DarkSwitch

---
+++ src/views/CollectionListView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+function toggleSidebar() {
+  let doc = document.getElementById("sidebar")
+  if (doc !== null) {
+    doc.classList.toggle("collapsed")
+  }  
+}
+</script>
+
+<template>
+    <button @click="toggleSidebar" id="close-sidebar" title="Close sidebar"></button>
+    <h1>Collection List</h1>
+</template>
+
+<style scoped>
+h1 {
+    text-align: center;
+}
+
+#close-sidebar {
+  float:right;
+}
+</style>
---
+++ src/views/FooterView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <h1>Footer</h1>
+</template>
+
+<style scoped>
+h1 {
+    text-align: center;
+}
+</style>
