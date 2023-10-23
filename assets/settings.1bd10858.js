cae937de57daa842a1cc20c2eafd720d88801820
sploit <acidsploit@users.noreply.github.com>
2023-10-23 22:29:06 +0200

    Updates

---
+++ assets/settings.1bd10858.js
@@ -0,0 +1,1 @@
+import{i as t,j as a}from"./index.edb9b79d.js";const r=localStorage.getItem("chaingraphuri"),n=r||"https://gql.chaingraph.pat.mn/v1/graphql",i=t("settings",()=>({chaingraphUrl:a(n)}));export{i as u};
