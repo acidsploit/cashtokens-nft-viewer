f6dc5457edbc7d103d2751fa148307e61a07c7e4
sploit <acidsploit@users.noreply.github.com>
2023-10-30 22:38:40 +0100

    Add NFTDetails view 	modified:   src/views/FooterView.vue 	new file:   src/views/NFTDetailsView.vue

--- src/views/FooterView.vue
+++ src/views/FooterView.vue
@@ -5,15 +5,10 @@
   <div class="container footer">
     <div class="content first">
       <p>
-        By <a href="https://twitter.com/acidsploit" target="_blank">sploit</a> with <span class="heart">❤️</span>
-      </p>
-
-      <p>
         Building and maintaining a project on a fast and progressively changing platform like Bitcoin Cash requires
         continious maintenance and updates.
         To offset this cost I would greatly appreciate any change you could chuck my way.
       </p>
-
       <p>
         CashAddress<br />
         <a href="bitcoincash:qrnml95v6wmduw6xzwvddph4cevahxzzhq9n56y2sv">bitcoincash:qrnml95v6wmduw6xzwvddph4cevahxzzhq9n56y2sv</a>
@@ -21,6 +16,9 @@
       <p>
         TokenAddress<br />
         <a href="bitcoincash:zrnml95v6wmduw6xzwvddph4cevahxzzhqze8y2v0l">bitcoincash:zrnml95v6wmduw6xzwvddph4cevahxzzhqze8y2v0l</a>
+      </p>
+      <p>
+        By <a href="https://twitter.com/acidsploit" target="_blank">sploit</a> with <span class="heart">❤️</span>
       </p>
     </div>
     <div class="content second">
---
+++ src/views/NFTDetailsView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const props = defineProps({
+  address: { type: String, required: true },
+  tokenId: { type: String, required: true },
+  commitment: {type: String, required: true},
+})
+
+</script>
+
+<template>
+<h1>NFT Details</h1>
+<p>{{ props.address }}</p>
+<p>{{ props.tokenId }}</p>
+<p>{{ props.commitment }}</p>
+</template>
+
+<style scoped>
+</style>
