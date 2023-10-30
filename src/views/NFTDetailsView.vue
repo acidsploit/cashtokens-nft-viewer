bba6e3c49b9701cddac3da7d4cc0222043004d8d
sploit <acidsploit@users.noreply.github.com>
2023-10-30 23:48:46 +0100

    get me some data

--- src/views/NFTDetailsView.vue
+++ src/views/NFTDetailsView.vue
@@ -1,8 +1,25 @@
 <script setup lang="ts">
+import { useSearchStore } from '@/stores/search';
+import type { Token } from '@/utils';
+import { onMounted, ref } from 'vue';
+
 const props = defineProps({
   address: { type: String, required: true },
   tokenId: { type: String, required: true },
   commitment: {type: String, required: true},
+})
+
+const search = useSearchStore()
+const collection = ref(null as Token | null)
+
+onMounted(() => {
+  if (search.result.address === props.address) {
+    let searchCollection = search.result.tokens.find((token) => {return token.id === props.tokenId})
+    if (searchCollection != undefined) {
+      console.log("we have data")
+      collection.value = searchCollection
+    }
+  }
 })
 
 </script>
@@ -12,7 +29,18 @@ const props = defineProps({
 <p>{{ props.address }}</p>
 <p>{{ props.tokenId }}</p>
 <p>{{ props.commitment }}</p>
+<pre class="mono data">
+  {{ JSON.stringify(collection?.bcmr?.token?.nfts?.parse.types[props.commitment], null, 4) }}
+</pre>
 </template>
 
 <style scoped>
+.mono {
+  font-family: monospace;
+}
+
+.data {
+  word-wrap: break-word;
+  max-width: 60vw;
+}
 </style>
